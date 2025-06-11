55c0fb2ee137a34ec332de069fd188139c8d2eaa
aditya24024024 <aditya24024024@gmail.com>
2025-06-12 00:07:15 +0530

    10

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -10,6 +10,7 @@ export const DASHBOARD_DATA_ROUTES = `${API_URL}/dashboard`;
 
 export const SIGNUP_ROUTE = `${AUTH_ROUTES}/signup`;
 export const LOGIN_ROUTE = `${AUTH_ROUTES}/login`;
+export const LOGOUT_ROUTE = `${AUTH_ROUTES}/logout`;
 export const GET_USER_INFO = `${AUTH_ROUTES}/get-user-info`;
 export const SET_USER_INFO = `${AUTH_ROUTES}/set-user-info`;
 export const SET_USER_IMAGE = `${AUTH_ROUTES}/set-user-image`;
