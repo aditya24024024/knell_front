55798a9c867e98e6e63f1d1a45da320403efaac4
aditya24024024 <aditya24024024@gmail.com>
2025-06-13 21:47:37 +0530

    Update Joinknell.jsx

--- client/src/components/Landing/Joinknell.jsx
+++ client/src/components/Landing/Joinknell.jsx
@@ -1,24 +1,31 @@
 import Image from "next/image";
 import React from "react";
 
-function Joinknell(){
-    return (
-        <div className="mx-4 sm:mx-12 md:mx-20 lg:mx-32 my-16 relative">
-            <div className="absolute z-10 top-1/4 sm:top-1/3 left-4 sm:left-10 w-[90%] sm:w-auto">
-                <h4 className="text-white text-3xl sm:text-4xl md:text-5xl mb-6 sm:mb-10">
-                    suddenly it&apos;s all so <i>doable.</i>
-                </h4>
-                <button
-                    className="border text-base font-medium px-5 py-2 border-{#1DBF73} bg-{#1DBF73} text-white rounded-md"
-                    type="button">
-                        Join Knell
-                </button>
-            </div>
-            <div cclassName="w-full h-64 sm:h-80 md:h-96 relative">
-                <Image src="/bg-signup.webp" fill alt="signup" className="rounded-sn"/>
-            </div>
-        </div>
-    );
+function Joinknell() {
+  return (
+    <div className="mx-4 sm:mx-12 md:mx-20 lg:mx-32 my-16 relative">
+      <div className="absolute z-10 top-1/4 sm:top-1/3 left-4 sm:left-10 w-[90%] sm:w-auto">
+        <h4 className="text-white text-3xl sm:text-4xl md:text-5xl mb-6 sm:mb-10">
+          suddenly it&apos;s all so <i>doable.</i>
+        </h4>
+        <button
+          className="border text-base font-medium px-5 py-2 border-[#1DBF73] bg-[#1DBF73] text-white rounded-md"
+          type="button"
+        >
+          Join Knell
+        </button>
+      </div>
+
+      <div className="w-full h-64 sm:h-80 md:h-96 relative">
+        <Image
+          src="/bg-signup.webp"
+          fill
+          alt="signup"
+          className="rounded-md object-cover"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Joinknell;
