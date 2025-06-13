6776788a63cc021386df19541a2fc94088cc7d15
aditya24024024 <aditya24024024@gmail.com>
2025-06-13 23:46:50 +0530

    17

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -6,12 +6,12 @@ import Joinknell from "../components/Landing/Joinknell";
 import PopularServices from "../components/Landing/PopularServices";
 import Services from "../components/Landing/Services";
 import React from "react";
+import { useCookies } from 'react-cookie';
 
 function Index() {
+  const [cookies] = useCookies();
   return (
-    
     <div>
-      
       <HeroBanner/>
       <Companies/>
       <PopularServices/>
