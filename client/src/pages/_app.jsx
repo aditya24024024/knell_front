9d609e740519306602fc9aa67a136b51026c8c77
aditya24024024 <aditya24024024@gmail.com>
2025-06-13 16:25:07 +0530

    Update _app.jsx

--- client/src/pages/_app.jsx
+++ client/src/pages/_app.jsx
@@ -7,7 +7,7 @@ import Head from "next/head";
 import { useRouter } from "next/router";
 import Navbar from "../components/Navbar";
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css"; // Import the toastify styles
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
@@ -17,15 +17,21 @@ export default function App({ Component, pageProps }) {
       <Head>
         <link rel="shortcut icon" href="/unnamed.jpg" />
         <title>knell</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <div className="relative flex flex-col h-screen justify-between">
+      <div className="relative flex flex-col min-h-screen">
         <Navbar />
-        <main className={`mb-auto w-full mx-auto ${router.pathname !== "/" ? "mt-36" : ""}`}>
+        <main
+          className={`mb-auto w-full px-4 md:px-10 mx-auto ${
+            router.pathname !== "/" ? "mt-20 md:mt-36" : ""
+          }`}
+        >
           <Component {...pageProps} />
         </main>
         <Footer />
-        <ToastContainer position="top-right" autoClose={3000} /> {/* 🔔 Popups here */}
+        <ToastContainer position="top-right" autoClose={3000} />
       </div>
     </StateProvider>
   );
 }
+
