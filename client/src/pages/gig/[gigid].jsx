7a77c080e8528eb47beb1d728891cefa8e45084f
aditya24024024 <aditya24024024@gmail.com>
2025-06-18 19:27:43 +0530

    Update [gigid].jsx

--- client/src/pages/gig/[gigid].jsx
+++ client/src/pages/gig/[gigid].jsx
@@ -5,56 +5,55 @@ import { useStateProvider } from '../../context/StateContext';
 import { CHECK_USER_ORDERED_GIG_ROUTE, GET_GIG_DATA } from '../../utils/constants';
 import axios from 'axios';
 import { useRouter } from 'next/router';
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
 
 const Gig = () => {
-    const router = useRouter();
-    const {gigid}=router.query
-    // {console.log({gigid})}
-    const [{  gigData,userInfo }, dispatch] = useStateProvider();
-    useEffect(() => {
-      dispatch({ type: reducerCases.SET_GIG_DATA, gigData: undefined });
-    }, [dispatch]);
-    useEffect(() => {
-      const fetchGigData = async () => {
-        try {
-          const {
-            data: { gig },
-          } = await axios.get(`${GET_GIG_DATA}/${gigid}`);
-          dispatch({ type: reducerCases.SET_GIG_DATA, gigData: gig });
-          // console.log({gig})
-          // console.log("edrtfgyhujikl")
-        } catch (err) {
-          console.log(err);
-        }
-      };
-      if (gigid) {
-        fetchGigData();}
-    }, [gigid, dispatch]);
-  
-    useEffect(() => {
-      const checkGigOrdered = async () => {
-        const {
-          data: { hasUserOrderedGig },
-        } = await axios.get(`${CHECK_USER_ORDERED_GIG_ROUTE}/${gigid}`, {
+  const router = useRouter();
+  const { gigid } = router.query;
+  const [{ gigData, userInfo }, dispatch] = useStateProvider();
+
+  // Clear gig data on initial render
+  useEffect(() => {
+    dispatch({ type: reducerCases.SET_GIG_DATA, gigData: undefined });
+  }, [dispatch]);
+
+  // Fetch gig data when gigid is available
+  useEffect(() => {
+    const fetchGigData = async () => {
+      try {
+        const response = await axios.get(`${GET_GIG_DATA}/${gigid}`);
+        dispatch({ type: reducerCases.SET_GIG_DATA, gigData: response.data.gig });
+      } catch (err) {
+        console.error("Failed to fetch gig data:", err);
+      }
+    };
+    if (gigid) fetchGigData();
+  }, [gigid, dispatch]);
+
+  // Check if the current user has already ordered this gig
+  useEffect(() => {
+    const checkGigOrdered = async () => {
+      try {
+        const response = await axios.get(`${CHECK_USER_ORDERED_GIG_ROUTE}/${gigid}`, {
           withCredentials: true,
         });
         dispatch({
           type: reducerCases.HAS_USER_ORDERED_GIG,
-          hasOrdered: hasUserOrderedGig,
+          hasOrdered: response.data.hasUserOrderedGig,
         });
-      };
-      if (userInfo) {
-        checkGigOrdered();
+      } catch (err) {
+        console.error("Failed to check gig order status:", err);
       }
-    }, [dispatch, gigid, userInfo]);
-  
-    return (
-      <div className="grid grid-cols-3 mx-32 gap-20">
-        <Details />
-        <Pricing />
-      </div>
-    );
-}
+    };
+    if (userInfo && gigid) checkGigOrdered();
+  }, [gigid, userInfo, dispatch]);
 
-export default Gig
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-3 mx-6 md:mx-16 lg:mx-32 gap-10 md:gap-16 lg:gap-20">
+      <Details />
+      <Pricing />
+    </div>
+  );
+};
+
+export default Gig;
