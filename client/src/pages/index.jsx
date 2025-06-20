763c6adbcd9df7b935cc952f291a545318f217e7
aditya24024024 <aditya24024024@gmail.com>
2025-06-20 23:17:51 +0530

    redirect_check

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -5,11 +5,29 @@ import HeroBanner from "../components/Landing/HeroBanner";
 import Joinknell from "../components/Landing/Joinknell";
 import PopularServices from "../components/Landing/PopularServices";
 import Services from "../components/Landing/Services";
-import React from "react";
-import { useCookies } from 'react-cookie';
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
+import { toast } from "react-toastify";
 
 function Index() {
-  const [cookies] = useCookies();
+  const router=useRouter();
+  const redirected=router.query.redirected;
+  useEffect(() => {
+    if (redirected === 'true') {
+      toast.warn("Please login/signup first")
+      // Create a new query object without 'redirected'
+      const { redirected, ...restQuery } = router.query;
+
+      router.replace(
+        {
+          pathname: router.pathname,
+          query: restQuery, // cleaned-up query
+        },
+        undefined,
+        { shallow: true } // prevents full page reload
+      );
+    }
+  }, [redirected, router]);
   return (
     <div>
       <HeroBanner/>
