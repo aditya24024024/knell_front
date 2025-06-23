f7d62a15ca037cc1ab512e52646eac1c6cb8eb38
aditya24024024 <aditya24024024@gmail.com>
2025-06-24 01:46:58 +0530

    middleware_creation

--- client/src/middleware.js
+++ client/src/middleware.js
@@ -2,10 +2,10 @@
 import { NextResponse } from 'next/server';
 
 export async function middleware(request) {
-  const token = request.cookies.get('token')?.value;
+  const token = request.cookies.get('jwt')?.value;
   console.log("gyuvifj",request);
   console.log("hfj",request?.cookies);
-  console.log("tguirr",request?.cookies?.get('token'));
+  console.log("tguirr",request?.cookies?.get('jwt'));
 
   if (!token) {
     return NextResponse.redirect(new URL('/', request.url));
