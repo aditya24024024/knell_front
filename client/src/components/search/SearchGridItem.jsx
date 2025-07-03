d4d0e10c9e8661fad4ebc01a6fbc4de277b398ee
aditya24024024 <aditya24024024@gmail.com>
2025-07-03 23:05:50 +0530

    addin_cloudinary

--- client/src/components/search/SearchGridItem.jsx
+++ client/src/components/search/SearchGridItem.jsx
@@ -30,7 +30,7 @@ const SearchGridItem = ({ gig }) => {
       {/* Gig Image */}
       <div className="relative w-full h-40 sm:h-48 md:h-52 rounded-xl overflow-hidden">
         <Image
-          src={`${IMAGES_URL}/${gig.images[0]}`}
+          src={`${gig.images[0]}`}
           alt="Gig Image"
           fill
           className="object-cover"
