b68240b83c6e9c310bc0a91b0feea440ef7a7937
aditya24024024 <aditya24024024@gmail.com>
2025-07-04 09:31:02 +0530

    addin_cloudinary

--- client/src/pages/seller/gigs/[gigid].jsx
+++ client/src/pages/seller/gigs/[gigid].jsx
@@ -107,7 +107,7 @@ const Editgig = () => {
         setFeatures(gig.features || []);
 
         gig.images.forEach((image) => {
-          const url = `${HOST}/uploads/${image}`;
+          const url = `${image}`;
           const fileName = image;
           fetch(url).then(async (response) => {
             const contentType = response.headers.get('content-type');
