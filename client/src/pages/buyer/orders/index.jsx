08aa97de547571baf67cd90d0ef21b0ee8b191e3
aditya24024024 <aditya24024024@gmail.com>
2025-07-05 01:52:39 +0530

    Update index.jsx

--- client/src/pages/buyer/orders/index.jsx
+++ client/src/pages/buyer/orders/index.jsx
@@ -132,6 +132,7 @@ function BuyerOrders() {
         const { data } = await axios.get(GET_BUYER_ORDERS_ROUTE, {
           withCredentials: true,
         });
+        console.log(data);
         setOrders(data.orders || []);
         setHasFetched(true);
       } catch (error) {
