7dfd86ca40d3fbd79195fd07bfb8b3d198498a0d
aditya24024024 <aditya24024024@gmail.com>
2025-09-01 20:33:13 +0530

    Update SearchGridItem.jsx

--- client/src/components/search/SearchGridItem.jsx
+++ client/src/components/search/SearchGridItem.jsx
@@ -34,6 +34,7 @@ const SearchGridItem = ({ gig }) => {
           alt="Gig Image"
           fill
           className="object-cover"
+          loading="lazy"
         />
       </div>
 
@@ -46,6 +47,7 @@ const SearchGridItem = ({ gig }) => {
             height={30}
             width={30}
             className="rounded-full object-cover"
+            loading="lazy"
           />
         ) : (
           <div className="bg-purple-500 h-[30px] w-[30px] flex items-center justify-center rounded-full">
