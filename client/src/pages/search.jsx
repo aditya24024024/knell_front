2a133765f217421ee598b123a8df1858bbd2c7f3
aditya24024024 <aditya24024024@gmail.com>
2025-09-01 20:48:40 +0530

    Update search.jsx

--- client/src/pages/search.jsx
+++ client/src/pages/search.jsx
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 import axios from "axios";
 import { SEARCH_GIGS_ROUTE } from '../utils/constants';
 import SearchGridItem from '../components/search/SearchGridItem';
+import dynamic from 'next/dynamic';
 
 const Search = () => {
   const router = useRouter();
