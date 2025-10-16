cc7efaf6f03e680fc1af4ed0d0f57eafa9cccdef
aditya24024024 <aditya24024024@gmail.com>
2025-10-17 01:31:24 +0530

    Update MessageContainer.jsx

--- client/src/components/Messages/MessageContainer.jsx
+++ client/src/components/Messages/MessageContainer.jsx
@@ -89,7 +89,7 @@ function MessageContainer() {
     console.log(lastSentRef.current);
     console.log(now);
     console.log(lastSentRef.current && now - lastSentRef.current)
-  if (now - lastSentRef.current < 10000) {
+  if (now - lastSentRef.current < 100000) {
     console.warn("You're sending messages too quickly!");
     return;
   }
