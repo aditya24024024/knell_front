eed8de5bbed2519361a386ce986d6841abd51bad
aditya24024024 <aditya24024024@gmail.com>
2025-11-03 18:26:58 +0530

    Update [gigid].jsx

--- client/src/pages/gig/[gigid].jsx
+++ client/src/pages/gig/[gigid].jsx
@@ -53,11 +53,9 @@ const Gig = () => {
     <Head>
       <meta name="keywords" content={gigData.features?.join(', ') || ''} />
     </Head>
-    // <div>{gigData?.features?.join(', ') || ''}
     <div className="grid grid-cols-1 lg:grid-cols-3 mx-6 md:mx-16 lg:mx-32 gap-10 md:gap-16 lg:gap-20">
       <Details />
       <Pricing />
-    </div>
     </div>
   );
 };
