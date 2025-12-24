7abe58361da5373ef1accfacc696d718f7b89a6b
aditya24024024 <aditya24024024@gmail.com>
2025-12-24 14:03:34 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -234,7 +234,7 @@ const [{ showLoginModal, showSignupModal, isSeller, userInfo, hamburger, otpmoda
 
   return (
     <>
-      (userInfo?.email) && !(userInfo?.username) && <UsernameWrapper/>
+      {(userInfo?.email) && !(userInfo?.username) && <UsernameWrapper/>}
       {/* showUsernameModal && <UsernameWrapper/> */}
       {showLoginModal && <AuthWrapper type="login" />}
       {(showSignupModal || resetPass) && <AuthWrapper type="signup" />}
