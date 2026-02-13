a4d57ae1417b8dacc52de825410a2c53dd53678f
aditya24024024 <aditya24024024@gmail.com>
2026-02-13 22:22:07 +0530

    Update next.config.mjs

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,14 +1,29 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images:{
+//     remotePatterns:[{
+//       protocol:"https",
+//       hostname:"**",
+//       port:"",
+//       pathname:"**",
+//     }]
+//   }
+// };
+
+// export default nextConfig; because site fatrrii thi and next img optimizer messing w cloudonary
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images:{
-    remotePatterns:[{
-      protocol:"https",
-      hostname:"**",
-      port:"",
-      pathname:"**",
-    }]
-  }
+  images: {
+    unoptimized: true, // important fix
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
