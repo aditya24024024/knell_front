1c6652ed4a4cddd402386f11aafa3cac1c29d446
aditya24024024 <aditya24024024@gmail.com>
2026-03-04 22:42:36 +0530

    Update categories.js

--- client/src/utils/categories.js
+++ client/src/utils/categories.js
@@ -5,7 +5,7 @@ export const categories = [
     { name: "Gaming Companion ", logo: "/service-11.svg" },
     { name: "Pet Companion ", logo: "/service-13.svg" },
     { name: "Old Age Companion", logo: "/service-15.svg" },
-    { name: "Freelancer", logo: "/service-6.svg" },
+    { name: "Freelancer", logo: "/service-24.svg" },
     { name: "Travel Companion", logo: "/service-14.svg" },
     { name: "Pub Crawler", logo: "/service-19.svg" },
     { name: "Dance Companion", logo: "/service-18.svg" },
@@ -13,7 +13,7 @@ export const categories = [
     { name: "Shopper", logo: "/service-12.svg" },
     { name: "Musician", logo: "/service-20.svg" },
     { name: "editor", logo: "/service-23.svg" },
-    { name: "Online Meet", logo: "/service-24.svg" },
+    { name: "Online Meet", logo: "/service-6.svg" },
    
     
   ];
